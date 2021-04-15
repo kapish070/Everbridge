ce91b7e1f19513ce8c244ed6933d7efc7f6a7e9e
Kapish Bhardwaj <kapish070@gmail.com>
2021-04-15 19:06:49 +0530

    Demo project initialized

---
+++ Everbridge/src/main/java/com/example/Everbridge/EverbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Everbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverbridgeApplication.class, args);
+	}
+
+}
